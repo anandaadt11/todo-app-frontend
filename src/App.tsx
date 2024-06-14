cfb9d97b1dd1db5cc370d89d60ed095c72a1f056
Ananda Aditya <anandaadityaputra11@gmail.com>
2024-06-14 10:38:23 +0700

    initial push

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import { SyntheticEvent, useState } from "react";
+import Card from "./components/Card";
+import axios from "axios";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const submitData = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8080/api", {
+        title,
+        description,
+      });
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+      console.error("Error create data:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="w-[50%] min-h-screen m-auto ">
+        <h1 className="pt-5 text-7xl font-bold text-center ">
+          <span className=" bg-gradient-to-r from-lime-500  to-sky-500 bg-clip-text text-transparent">
+            Todo App
+          </span>
+        </h1>
+        <div className="border border-sky-300 rounded-lg mt-5 p-5 shadow-sm shadow-lime-500 ">
+          <h1 className="text-lime-500 text-2xl mb-1 font-bold">Create todo</h1>
+          <form onSubmit={submitData}>
+            <label htmlFor="Title">
+              <span className="block font-semibold mb-1 text-slate-700">
+                Title
+              </span>
+              <input
+                className="px-3 py-2 border shadow rounded w-full block text-sm placeholder:text-slate-400
+              focus:outline-none 
+              focus:ring-1 focus:ring-lime-500
+              focus:border-sky-500
+              "
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                type="text"
+                id="Title"
+                placeholder="Title"
+              />
+            </label>
+            <label htmlFor="Description">
+              <span className="block font-semibold mb-1 text-slate-700">
+                Description
+              </span>
+              <textarea
+                className="px-3 py-2 border shadow rounded w-full block text-sm placeholder:text-slate-400
+              focus:outline-none 
+              focus:ring-1 focus:ring-lime-500
+              focus:border-sky-500
+              "
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                id="Description"
+                placeholder="Description"
+              />
+            </label>
+            <button
+              className="bg-lime-500 p-2 text-sm rounded-lg font-medium hover:bg-lime-600 text-white shadow-lg mt-3 "
+              type="submit"
+            >
+              Create
+            </button>
+          </form>
+        </div>
+        <Card />
+        <div className=""></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  title: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+}
+
+const Card = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    getData();
+  }, [users]);
+
+  const getData: () => void = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api");
+      const sortedData = response.data.data.sort(
+        (a: User, b: User) =>
+          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      );
+      setUsers(sortedData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const delateData = async (id: number) => {
+    try {
+      axios.delete(`http://localhost:8080/api/${id}`);
+      getData();
+    } catch (error) {
+      console.error("Error delete data:", error);
+    }
+  };
+  return (
+    <>
+      {users.map((user) => (
+        <div
+          className="m-5 p-3 border-2 border-lime-500 rounded-xl hover:shadow-sm hover:shadow-sky-500"
+          key={user.id}
+        >
+          <h1 className="text-2xl font-bold text-slate-700">{user.title}</h1>
+          <p className="text-slate-500">{user.description}</p>
+          <div className="flex justify-end gap-1">
+            <button
+              onClick={() => delateData(user.id)}
+              className="bg-sky-500 p-2 text-sm rounded-lg font-medium hover:bg-sky-600 text-white shadow-lg"
+            >
+              Done
+            </button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Card;
